eb78abe969f95394d410a216a6f9c0511389f987
chrischep <christinechepleting5@gmail.com>
2023-08-02 10:32:17 +0300

    add props

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import './index.css';
 
 
 export default function MyApp(){
-
-  return(
-    <div>
-      <h1>Number of likes</h1>
-      <MyButton/>
-      <MyButton/>
-    </div>
-  );
-}
-function MyButton(){
   const [count, setCount] = useState(0);
 
   function handleClick() {
     setCount (count + 1);
   }
   return(
-    <button className="btns" onClick={handleClick}>
+    <div>
+      <h1>Number of likes</h1>
+      <MyButton count={count} onclick={handleClick}/>
+      <MyButton count={count} onClick={handleClick}/>
+    </div>
+  );
+}
+function MyButton({count, onClick}){
+return(
+    <button className="btns" onClick={onClick}>
       Liked {count} times
     </button>
   );
 }
+
